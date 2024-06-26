f4d3323d66806e2d7230ac6682980b31a7109244
John Woodell <woodie@netpress.com>
2024-06-26 10:26:00 -0700

    Fix grading standard.

--- canvas_cc_gem_course.rb
+++ canvas_cc_gem_course.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 require "canvas_cc"
-require "dom"
 require "securerandom"
 require "tmpdir"
 
@@ -15,18 +14,20 @@ course.assignment_groups << ag
 
 gs = CanvasCc::CanvasCC::Models::GradingStandard.new
 gs.title = "Grading Standard"
+gs.data = [["A", 0.9], ["B", 0.8], ["C", 0.7], ["D", 0.6], ["E", 0.0]]
+gs.version = '2'
 gs.identifier = SecureRandom.hex
 course.grading_standards = [gs]
 
 [ # creats assignments with a mix of submission_types
   ["Known", "Displays", "online_text_entry"],
   ["Unknown", "Has no", "n/a"],
-  ["Missing", "Has no", nil]
+  ["Missing", "Displays", nil]
 ].each do |data|
   assignment = CanvasCc::CanvasCC::Models::Assignment.new
   assignment.assignment_group_identifier_ref = ag.identifier
   assignment.title = "Course with #{data[0]} Submission Type"
-  assignment.body = "#{data[1]} submission type".dom(:p)
+  assignment.body = "<p>#{data[1]} submission type</p>"
   assignment.submission_types << data[2] if data[2]
   assignment.points_possible = 10
   assignment.workflow_state = "active"
