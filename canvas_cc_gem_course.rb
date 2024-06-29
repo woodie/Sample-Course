d59666cfec7a1c5c7702911b9a7234aa6d60c743
John Woodell <woodie@netpress.com>
2024-06-29 16:31:52 -0700

    Use consistent and easy-to-read identifiers.

--- canvas_cc_gem_course.rb
+++ canvas_cc_gem_course.rb
@@ -1,37 +1,39 @@
 #!/usr/bin/env ruby
 require "canvas_cc"
-require "securerandom"
 require "tmpdir"
+
+@i = 0
+def new_identifier = (6000**10 + @i += 1).to_s(16)
 
 course = CanvasCc::CanvasCC::Models::Course.new
 course.title = "Canvas CC Gem Course"
-course.identifier = SecureRandom.hex
+course.identifier = new_identifier
 
 ag = CanvasCc::CanvasCC::Models::AssignmentGroup.new
 ag.title = "Assignment Group"
-ag.identifier = SecureRandom.hex
+ag.identifier = new_identifier
 course.assignment_groups << ag
 
 gs = CanvasCc::CanvasCC::Models::GradingStandard.new
 gs.title = "Grading Standard"
 gs.data = [["A", 0.9], ["B", 0.8], ["C", 0.7], ["D", 0.6], ["E", 0.0]]
-gs.version = '2'
-gs.identifier = SecureRandom.hex
+gs.version = "2"
+gs.identifier = new_identifier
 course.grading_standards = [gs]
 
 [ # creats assignments with a mix of submission_types
   ["Known", "online_text_entry"],
   ["Unknown", "n/a"],
   ["Missing", nil]
-].each do |data|
+].each do |label, type|
   assignment = CanvasCc::CanvasCC::Models::Assignment.new
   assignment.assignment_group_identifier_ref = ag.identifier
-  assignment.title = "Course with #{data.first} Submission Type"
+  assignment.title = "Course with #{label} Submission Type"
   assignment.body = "<p>This is assignment content.</p>"
-  assignment.submission_types << data.last unless data.last.nil?
+  assignment.submission_types << type unless type.nil?
   assignment.points_possible = 10
   assignment.workflow_state = "active"
-  assignment.identifier = SecureRandom.hex
+  assignment.identifier = new_identifier
   course.assignments << assignment
 end
 
